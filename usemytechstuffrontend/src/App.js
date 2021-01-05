de25a9f3a2d22509a9a2a76bb2d9a18f99ce1474
Louis Stevens <loustevenshere@gmail.com>
2021-01-05 13:06:02 -0500

    Login Page barebones structure

--- usemytechstuffrontend/src/App.js
+++ usemytechstuffrontend/src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import Login from './Components/Login'
+
+const change = () => {
+  return null
+}
+
+const submit = () => {
+  return null
+}
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+  <div className="App"></div>
+  <Login value=" " change={change} submit={submit}/>
+  </>
+
+  );
 }
 
 export default App;
--- usemytechstuffrontend/src/Components/Login.js
+++ usemytechstuffrontend/src/Components/Login.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
-const Login = () => {
+const Login = (props) => {
+    const {
+        value,
+        submit,
+        change
+    } = props
     return (
-        <div>
-
+        <div className='login-container'>
+            <h1>Login Page</h1>
+                <form className='login-form'>
+                    <label>
+                        Username
+                        <input
+                        value={value}
+                        onChange={change}
+                        name='username'
+                        type='text'
+                        placeholder='username'
+                        />
+                    </label>
+                    <label>
+                        Password
+                        <input
+                        value={value}
+                        onChange={change}
+                        name='password'
+                        type='text'
+                        placeholder='password'
+                        />
+                    </label>
+                    <div className='submit-btn'>
+                        <button>Submit</button>
+                    </div>
+                    <div className='register-text'>
+                        Dont have an account? <a href=" ">Register Here</a>
+                    </div>
+                </form>
         </div>
     )
 }
