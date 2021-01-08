e49bc1ec8a2a6f2f63f66c22feba818c366321a7
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-01-07 21:24:18 -0600

    "Chance: Linked our forms together, and added proper background color"

--- usemytechstuffrontend/src/Actions/Components/Login.js
+++ usemytechstuffrontend/src/Actions/Components/Login.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 // import Register from "./Register";
 // import './Login.css';
-
+import "./Register.css"
 // Styles
 
 const Container = styled.div`
